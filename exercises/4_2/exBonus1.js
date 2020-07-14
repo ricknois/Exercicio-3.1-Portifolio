c8072a98c45559488b7ce3c3a7b4bf5ea2191dfc
ricknois <rickssti@outlook.com>
2020-07-14 20:05:27 -0300

    Exercícios bonus

---
+++ exercises/4_2/exBonus1.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 19, 70, 8, 100, 35, 27];
+for (let i = 1; i < numbers.length; i+=1) {
+    for (let j = 0; j < i; j+=1) {
+        if (numbers[i] < numbers[j]) {
+            let aux = numbers[i];
+
+            numbers[i] = numbers [j];
+            numbers[j] = aux
+        }
+
+    }
+
+}
+console.log(numbers);
