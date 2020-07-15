41505eb029aba21233b712bd5c67129907f4be8f
ricknois <rickssti@outlook.com>
2020-07-15 18:07:30 -0300

    Exercicios 1,2,3,6

---
+++ exercises/4_3/ex1.js
@@ -0,0 +1,6 @@
+var num = 5;
+var ast = "*";
+
+for (let index = 0; index < num; index++) {
+    console.log(ast.repeat(num));
+}
---
+++ exercises/4_3/ex2.js
@@ -0,0 +1,7 @@
+let num = 10;
+let sim = "*";
+let str = "";
+for (let index = 0; index < num; index++) {
+    str += sim;
+    console.log(str);
+}
