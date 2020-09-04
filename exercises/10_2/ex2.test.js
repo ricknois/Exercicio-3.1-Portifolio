201bd9442e25bcd3bcec45230dca15e434c69cd2
ricknois <rickssti@outlook.com>
2020-09-04 03:37:15 -0300

    ...

---
+++ exercises/10_2/ex2.test.js
@@ -0,0 +1,33 @@
+const { italic } = require("chalk");
+const { exportAllDeclaration } = require("@babel/types");
+
+const users = {
+  4: { name: 'Mark' },
+  5: { name: 'Paul' }
+};
+
+const findUserById = (id) => {
+  return new Promise((resolve, reject) => {
+      if (users[id]) {
+        return resolve(users[id]);
+      }
+
+      return reject({ error: 'User with ' + id + ' not found.' });
+  });
+}
+
+const getUserName = (userID) => {
+  return findUserById(userID).then(user => user.name);
+}
+
+it('return user', () => {
+  return getUserName(4).then(usr => expect(usr).toEqual('Mark'))
+  });
+
+  it('return error', () => {
+    return getUserName(2).catch(err => expect(err).toEqual({ error: 'User with 2 not found.' }))
+  })
+
+
+//Utilizando a sintaxe de Promise, faça um teste que verifique o resultado da função getUserName para o caso em que o usuário é encontrado, e também um teste para o caso em que o usuário não é encontrado.
+//Dica: Veja os dados falsos utilizados no banco de dados, disponíveis na variável users, para saber quais IDs existem.
