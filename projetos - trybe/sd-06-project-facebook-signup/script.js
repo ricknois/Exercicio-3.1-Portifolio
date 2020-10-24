894e8cbbfe33d4d7fcf41df2e9b4056ab99779f6
Rick Sousa <rickssti@outlook.com>
2020-10-24 20:44:37 -0300

    :zap: Arquivos removidos

--- projetos - trybe/sd-06-project-facebook-signup/script.js
+++
@@ -1,8 +0,0 @@
-const btnLogin = document.querySelector('#button-login');
-
-
-btnLogin.addEventListener('click', function () {
-  if (document.getElementById('user-email-phone').value !== ' ') {
-    alert(document.getElementById('user-email-phone').value);
-  }
-});
